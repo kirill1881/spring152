29bdb816c5e603950eafc72bbf153d04dd78478c
kirill1881 <82600534+kirill1881@users.noreply.github.com>
2022-10-16 15:08:15 +0300

    Initial commit

---
+++ src/test/java/com/example/spring152/Spring152ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring152;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring152ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
