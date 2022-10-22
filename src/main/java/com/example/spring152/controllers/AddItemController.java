c28ef532eee062d494ea0957c07bee682a5a270e
kirill1881 <82600534+kirill1881@users.noreply.github.com>
2022-10-22 15:48:36 +0300

    1232

--- src/main/java/com/example/spring152/controllers/AddItemController.java
+++ src/main/java/com/example/spring152/controllers/AddItemController.java
@@ -1,14 +1,42 @@
 package com.example.spring152.controllers;
 
+import com.example.spring152.models.ItemModel;
+import com.example.spring152.repos.ItemRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/addItem")
 public class AddItemController {
+
+    final ItemRepo itemRepo;
+    private String name;
+
+    public AddItemController(ItemRepo itemRepo) {
+        this.itemRepo = itemRepo;
+    }
+
     @GetMapping
     public String getAddItemPage(){
         return "addItem";
+    }
+
+    @PostMapping
+    public RedirectView setData(@RequestParam String name,
+                                @RequestParam String price,
+                                @RequestParam String disc,
+                                @RequestParam String url){
+        ItemModel itemModel = new ItemModel();
+        itemModel.setDisc(disc);
+        itemModel.setName(name);
+        itemModel.setUrl(url);
+        itemModel.setPrice(price);
+
+        itemRepo.save(itemModel);
+        RedirectView redirectView = new RedirectView("/");
+
+        return redirectView;
     }
 }
--- src/main/java/com/example/spring152/repos/ItemRepo.java
+++ src/main/java/com/example/spring152/repos/ItemRepo.java
@@ -2,6 +2,8 @@ package com.example.spring152.repos;
 
 import com.example.spring152.models.ItemModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepo extends JpaRepository<ItemModel, Long> {
 }
